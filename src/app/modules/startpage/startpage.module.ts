2dcf7b16c6b3f9300cd7cefabf9b81a68114653d
selinaschindlauerprivat <selinaschindlauer@gmail.com>
2022-03-19 16:15:42 +0100

    chore: update

--- src/app/modules/startpage/startpage.module.ts
+++ src/app/modules/startpage/startpage.module.ts
@@ -1,18 +1,11 @@
-import { Input, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StartpageComponent } from './startpage.component';
-import { HeaderModule } from 'src/app/shared/modules/header/header.module';
-import { BarberModule } from 'src/app/shared/barber/barber.module';
+import { Input, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { StartpageComponent } from "./startpage.component";
+import { HeaderModule } from "src/app/shared/modules/header/header.module";
+import { BarberModule } from "src/app/shared/modules/barber/barber.module";
 
 @NgModule({
-
-  
-  imports: [
-    CommonModule, 
-    HeaderModule, 
-    BarberModule
-  ],
-  declarations: [StartpageComponent],
-
+    imports: [CommonModule, HeaderModule, BarberModule],
+    declarations: [StartpageComponent]
 })
-export class StartpageModule { }
+export class StartpageModule {}
